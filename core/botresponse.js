2b0b41c3fd4f76bf425503ec51486bbaed326761
AURA <fazrihafidz120@gmail.com>
2025-07-11 10:33:40 +0700

    AURABOT V1.0.0

--- core/botresponse.js
+++ core/botresponse.js
@@ -29,7 +29,6 @@ const handleTranslate = require('../commands/translate');
 const { addAdmin, removeAdmin } = require('../commands/admin');
 const buatGrup = require('../commands/buatGrup');
 const ekstrakAudio = require('../commands/ekstrakAudio');
-const ytmp3 = require('../commands/ytmp3');
 
 const greetedUsers = new Set()
 // const lastCommandMap = new Map()
@@ -143,9 +142,6 @@ if (text.startsWith('/') || text.startsWith('.')) {
     
     ✨ *Ketik sesuai yaa! Hindari typo biar nggak nyasar 😋*
     `
-     if (body.startsWith('.mp3')) {
-      return ytmp3(msg, sock);
-    }
 
     if (lowerText.startsWith('.na')) {
       return await addAdmin(sock, msg, sender, actualUserId, text);
