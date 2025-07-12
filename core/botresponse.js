9f6b5d5e19b0e34da59c399f2d2f817e4b781a77
AURA <fazrihafidz120@gmail.com>
2025-07-13 05:25:18 +0700

    AURABOT V1.0.0

--- core/botresponse.js
+++ core/botresponse.js
@@ -3,10 +3,33 @@ const userStateMap = new Map()
 const mutedUsers = new Map()
 const memoryMap = new Map()
 const muteDuration = 2 * 60 * 1000
+const add = require('../commands/add');
 // const { handleAutoKick } = require('../commands/auto_kick')
 const { handleStaticCommand } = require('../core/handler/staticCommand')
+const tagall = require('../commands/tagall')
+const kick = require('../commands/kick')
+const menfess = require('../commands/menfess')
 const handleWelcome = require('../commands/welcome');
+const { adminList, toxicWords } = require('../setting/setting')
 const askOpenAI = require('../core/utils/openai')
+const { createStickerFromMessage, createStickerFromText } = require('../core/stickerHelper')
+const downloadTiktok = require('../commands/tiktokDownloader');
+const downloadInstagram = require('../commands/igDownloader');
+const downloadYouTubeMP3 = require('../commands/youtubeDownloader');;
+const sendAll = require('../commands/sendAll');
+const showOnce = require('../commands/show');
+const handleQR = require('../commands/qris')
+const buatQR = require('../commands/createQr')
+const mapsQR = require('../commands/mapqr');
+const linkMap = require('../commands/linkmap');
+const waifuhen = require('../commands/waifuhen')
+const waifu = require('../commands/waifu')
+const stickerToMedia = require('../commands/stickerToMedia');
+const handleTranslate = require('../commands/translate');
+const { addAdmin, removeAdmin } = require('../commands/admin');
+const buatGrup = require('../commands/buatGrup');
+const ekstrakAudio = require('../commands/ekstrakAudio');
+const openCloseGroup = require('../commands/openCloseGroup');
 
 const greetedUsers = new Set()
 // const lastCommandMap = new Map()
@@ -49,11 +72,9 @@ async function handleResponder(sock, msg) {
 
     if (!text) return;
 
-    const handledStatic = await handleStaticCommand(sock, msg, lowerText, userId, from, body, text, command, args)
+     const handledStatic = await handleStaticCommand(sock, msg, lowerText, userId, from, body)
       if (handledStatic) return
 
-    // Anti-spam
-   // Hanya hitung spam jika dia mengirim command (pakai / atau . di awal)
 if (text.startsWith('/') || text.startsWith('.')) {
   const now = Date.now()
   const userSpam = spamTracker.get(userId) || []
@@ -89,6 +110,306 @@ if (text.startsWith('/') || text.startsWith('.')) {
 
     const handledMenfess = await menfess(sock, msg, text)
     if (handledMenfess) return
+    
+        const listBahasa = `🌐 *Daftar Kode Bahasa Umum:*
+    
+    🇮🇩 Indonesia — \`id\`  
+    🇬🇧 Inggris — \`en\`  
+    🇯🇵 Jepang — \`ja\`  
+    🇰🇷 Korea — \`ko\`  
+    🇨🇳 Mandarin — \`zh\`  
+    🇫🇷 Prancis — \`fr\`  
+    🇩🇪 Jerman — \`de\`  
+    🇪🇸 Spanyol — \`es\`  
+    🇷🇺 Rusia — \`ru\`  
+    🇮🇳 Hindi — \`hi\`  
+    🇹🇭 Thai — \`th\`  
+    🇵🇹 Portugis — \`pt\`  
+    🇮🇹 Italia — \`it\`  
+    🇳🇱 Belanda — \`nl\`  
+    🇹🇷 Turki — \`tr\`  
+    🇵🇱 Polandia — \`pl\`  
+    🇸🇦 Arab — \`ar\`  
+    🇻🇳 Vietnam — \`vi\`  
+    🇵🇭 Filipino — \`tl\`  
+    🇮🇱 Ibrani — \`he\`  
+    🇺🇦 Ukraina — \`uk\`
+    
+    📌 *Gunakan dengan perintah:*  
+    \`/tl <kode> <teks>\`  
+    Contoh: \`/tl en Saya lapar\` → akan diterjemahkan ke Inggris.
+    
+    ✨ *Ketik sesuai yaa! Hindari typo biar nggak nyasar 😋*
+    `
+    if (text.startsWith('.open') || text.startsWith('.close')) {
+      return await openCloseGroup(sock, msg, command);
+    }
+
+    if (lowerText.startsWith('.na')) {
+      return await addAdmin(sock, msg, sender, actualUserId, text);
+    }
+
+    if (lowerText.startsWith('.una')) {
+      return await removeAdmin(sock, msg, sender, actualUserId, text);
+    }
+
+    if (['/listbahasa', '.listbahasa', 'listbahasa', 'list bahasa'].includes(lowerText)) {
+      return sock.sendMessage(sender, {
+        text: listBahasa,
+      }, { quoted: msg });
+    }
+
+    if (command === '/tl' || command === '.tl' || command === 'tl') {
+      return await handleTranslate(sock, msg, text, command, args);
+    }
+
+    if (command === 'waifuhen') {
+      return await waifuhen(sock, msg, args.join(' '));
+    }
+
+
+    if (command === '.waifu') {
+      return await waifu(sock, msg, args.join(' '));
+    }
+
+    if (lowerText.startsWith('.sm') || lowerText.startsWith('sm')) {
+      await stickerToMedia(sock, msg);
+      return;
+    }
+    
+    if (lowerText.startsWith('.linkmap') || lowerText.startsWith('linkmap')) {
+      const isi = text.replace(/^\.*linkmap/i, '').trim();
+      return await linkMap(sock, msg, isi);
+    }
+
+   if (lowerText.startsWith('.mapqr') || lowerText.startsWith('mapqr')) {
+      const isi = text.replace(/^\.*mapqr/i, '').trim();
+      return await mapsQR(sock, msg, isi);
+    }
+
+
+    if (lowerText.startsWith('.qr') || lowerText.startsWith('qr')) {
+      return await handleQR(sock, msg);
+    }
+    
+    if (lowerText.startsWith('.cqr ') || lowerText.startsWith('cqr ')) {
+      const isiTeks = text.replace(/^\.*cqr/i, '').trim();
+      return await buatQR(sock, msg, isiTeks)
+    }
+
+    if (lowerText.startsWith('.show') || lowerText.startsWith('show')) {
+      return await showOnce(sock, msg);
+    }
+    if (body.startsWith('.sendAll')) {
+      const pesan = body.split(' ').slice(1).join(' ');
+      if (!pesan) return sock.sendMessage(from, { text: '❌ Format: .sendAll isi pesan' }, { quoted: msg });
+    
+      await sock.sendMessage(from, { text: '🔄 Mengirim ke semua kontak yang 1 grup...' }, { quoted: msg });
+      await sendAll(sock, sender, pesan);
+      await sock.sendMessage(from, { text: '✅ Pesan berhasil dikirim!' }, { quoted: msg });
+    }
+    
+    if (
+      body.trim().toLowerCase() === 'ets' &&
+      (
+        msg.message?.videoMessage ||
+        msg.message?.extendedTextMessage?.contextInfo?.quotedMessage?.videoMessage
+      )
+    ) {
+      return await ekstrakAudio(sock, msg);
+    }
+
+    if (text.startsWith('.dyts ')) {
+      const url = text.split('.dyts ')[1].trim();
+      try {
+        await sock.sendMessage(from, { text: '🎧 Mengunduh audio dari YouTube...' }, { quoted: msg });
+        const { filePath, title } = await downloadYouTubeMP3(url);
+        await sock.sendMessage(from, {
+          audio: { url: filePath },
+          mimetype: 'audio/mp4',
+          ptt: false
+        }, { quoted: msg });
+        fs.unlinkSync(filePath);
+      } catch (e) {
+        console.error('❌ Gagal download audio:', e);
+        await sock.sendMessage(from, { text: `❌ Gagal download audio:\n${e.message}` }, { quoted: msg });
+      }
+    }
+    
+          if (text.startsWith('.d ')) {
+          const link = text.split(' ')[1];
+          
+          if (!link || !link.includes('tiktok.com')) {
+            await sock.sendMessage(from, {
+              text: '❌ Link TikTok tidak valid!',
+            }, { quoted: msg });
+            return;
+          }
+        
+          await sock.sendMessage(from, {
+            text: '⏳ Sedang memproses link TikTok...',
+          }, { quoted: msg });
+        
+          try {
+            const result = await downloadTiktok(link);
+        
+            if (!result) {
+              await sock.sendMessage(from, {
+                text: '❌ Gagal mengambil data dari TikTok.',
+              }, { quoted: msg });
+              return;
+            }
+        
+            if (result.isPhoto && result.images.length > 0) {
+              await sock.sendMessage(from, {
+                text: '📷 Link kamu adalah Foto. ⬇️ Sedang Mengunduh...',
+              }, { quoted: msg });
+        
+              for (const imageUrl of result.images) {
+                await sock.sendMessage(from, {
+                  image: { url: imageUrl },
+                }, { quoted: msg });
+              }
+        
+            } else if (result.videoUrl) {
+              await sock.sendMessage(from, {
+                text: '🎞️ Link kamu adalah video. ⬇️ Sedang Mengunduh...',
+              }, { quoted: msg });
+        
+              await sock.sendMessage(from, {
+                video: { url: result.videoUrl },
+              }, { quoted: msg });
+        
+            } else {
+              await sock.sendMessage(from, {
+                text: '❌ Tidak ada media yang bisa diunduh dari link ini, Pastikan Link nya benar.',
+              }, { quoted: msg });
+            }
+        
+          } catch (e) {
+            console.error('❌ Error TikTok:', e);
+            await sock.sendMessage(from, {
+              text: '⚠️ Terjadi kesalahan saat memproses link TikTok.',
+            }, { quoted: msg });
+          }
+        
+          return;
+        }
+
+        if (text.startsWith('.ds ')) {
+          const link = text.split(' ')[1]
+        
+          if (!link || !link.includes('tiktok.com')) {
+            await sock.sendMessage(from, { text: '❌ Link TikTok tidak valid!' }, { quoted: msg })
+            return
+          }
+        
+          await sock.sendMessage(from, { text: '🎧 Mengunduh sound TikTok...' }, { quoted: msg })
+        
+          try {
+            const result = await downloadTiktok(link)
+            if (!result || !result.musicUrl) {
+              await sock.sendMessage(from, { text: '❌ Gagal mengunduh sound.' }, { quoted: msg })
+              return
+            }
+        
+            await sock.sendMessage(from, {
+              audio: { url: result.musicUrl },
+              mimetype: 'audio/mp4'
+            }, { quoted: msg })
+          } catch (e) {
+            console.error('❌ Error:', e)
+            await sock.sendMessage(from, { text: '⚠️ Error saat unduh sound.' }, { quoted: msg })
+          }
+        
+          return
+        }
+
+
+        if (text.startsWith('.dig ')) {
+      const link = text.split(' ')[1];
+    
+      if (!link || !link.includes('instagram.com')) {
+        await sock.sendMessage(from, { text: '❌ Link Instagram tidak valid!' }, { quoted: msg });
+        return;
+      }
+    
+      await sock.sendMessage(from, { text: '⏳ Sedang mengunduh video Instagram...' }, { quoted: msg });
+    
+      try {
+        const result = await downloadInstagram(link);
+        if (!result || !result.videoUrl) {
+          await sock.sendMessage(from, { text: '❌ Gagal mengunduh video Instagram.' }, { quoted: msg });
+          return;
+        }
+    
+        await sock.sendMessage(from, {
+          video: { url: result.videoUrl }
+        }, { quoted: msg });
+      } catch (e) {
+        console.error('❌ Error IG:', e);
+        await sock.sendMessage(from, { text: '⚠️ Terjadi kesalahan saat mengunduh Instagram.' }, { quoted: msg });
+      }
+    
+      return;
+    }
+    
+    if (lowerText.startsWith('kick') || lowerText.startsWith('kik') || lowerText.startsWith('.kick') || lowerText.startsWith('.kik')) {
+      return await kick(sock, msg, text, isGroup);
+    }
+
+    if (lowerText.startsWith('.tagall') || lowerText.startsWith('tagall') || lowerText.startsWith('tag semua') || lowerText.startsWith('tag')) {
+      return await tagall(sock, msg, text, isGroup);
+    }
+
+    if (
+      lowerText.startsWith('.add') ||
+      lowerText.startsWith('add') ||
+      lowerText.startsWith('tambah')
+    ) {
+      const raw = text.split(' ').slice(1).join(' ');
+      const nomorList = raw.split(',').map(n => {
+        let num = n.trim();
+        if (num.startsWith('0')) num = '62' + num.slice(1);
+        return num;
+      });
+      return await add(sock, msg, nomorList, sender, userId);
+    }
+    
+
+
+
+   if (['s', 'sticker'].includes(lowerText)) {
+      try {
+        const quoted = msg.message?.extendedTextMessage?.contextInfo?.quotedMessage
+        const hasMediaQuoted = quoted?.imageMessage || quoted?.videoMessage
+
+        const hasMediaDirect = msg.message?.imageMessage || msg.message?.videoMessage
+        const caption = msg.message?.imageMessage?.caption || msg.message?.videoMessage?.caption || ''
+
+        // Kalau pakai caption langsung "s"
+        const captionMatch = ['s', 'sticker'].includes(caption.toLowerCase())
+
+        if (hasMediaQuoted || captionMatch) {
+          await createStickerFromMessage(sock, msg)
+        } else {
+          await sock.sendMessage(sender, {
+            text: 'Kirim gambar/video lalu reply dengan "s", atau kirim gambar/video langsung dengan caption "s" atau "sticker"',
+          }, { quoted: msg })
+        }
+      } catch (err) {
+        console.error('❌ Gagal buat stiker:', err)
+        await sock.sendMessage(sender, { text: 'Ups! Gagal bikin stiker 😖 Coba lagi ya~' }, { quoted: msg })
+      }
+      return
+    }
+
+    if (command === 'stickertext' || command === 'st') {
+      if (!args[0]) return sock.sendMessage(sender, { text: 'Ketik: stikertext Halo dunia!' }, { quoted: msg })
+      const text = args.join(' ')
+      const stickerBuffer = await createStickerFromText(text)
+      await sock.sendMessage(sender, { sticker: stickerBuffer }, { quoted: msg })
+    }
 
     if (text === '.reset') {
       if (!adminList.includes(actualUserId)) {
@@ -102,8 +423,6 @@ if (text.startsWith('/') || text.startsWith('.')) {
         text: '🧠 Ingatan AuraBot Telah Direset, Ayo Buat Obrolan Baru~!'
       }, { quoted: msg });
     }
-    if (!text.startsWith('/')) {
-
     if (text.startsWith('/') && !['/menu', '/reset', '/riwayat', '/clear'].includes(lowerText)) {
       return sock.sendMessage(sender, { text: 'Maaf, aku gak ngerti perintah itu 😵. Coba ketik /menu yaa!' }, { quoted: msg })
     }
