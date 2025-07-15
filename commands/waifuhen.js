c2cf239e304bccfd08d44c0c26188c6ee30b47f8
AURA <fazrihafidz120@gmail.com>
2025-07-16 00:42:01 +0700

    AURABOT V1.0.0

--- commands/waifuhen.js
+++ commands/waifuhen.js
@@ -1,5 +1,7 @@
 const { adminList } = require('../setting/setting');
 const axios = require('axios');
+const path = require('path');
+const fs = require('fs');
 
 const allowedNSFW = ['ass', 'hentai', 'milf', 'oral', 'paizuri', 'ecchi', 'ero'];
 
