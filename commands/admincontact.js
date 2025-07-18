073bfe1b879b80d6c95cbe2d364ee8802f625448
AURA <fazrihafidz120@gmail.com>
2025-07-19 03:50:40 +0700

    AURABOT V1.0.0

--- commands/admincontact.js
+++ commands/admincontact.js
@@ -1,10 +1,4 @@
 module.exports = async (sock, msg) => {
-  const vcard = `BEGIN:VCARD
-VERSION:3.0
-FN:Aura Developer
-TEL;type=CELL;type=VOICE;waid=62895326679840:62895326679840
-END:VCARD`;
-
   await sock.sendMessage(
     msg.key.remoteJid,
     {
@@ -12,7 +6,14 @@ END:VCARD`;
         displayName: 'Aura Developer',
         contacts: [
           {
-            vcard
+            displayName: 'Aura Developer',
+            vcard: `
+BEGIN:VCARD
+VERSION:3.0
+FN:Aura Developer
+N:AURA
+TEL;type=CELL;type=VOICE;waid=62895326679840:62895326679840
+END:VCARD`
           }
         ]
       }
