993b281166f1d15844b34c9ea1a70204210015f4
AURA <fazrihafidz120@gmail.com>
2025-07-19 15:17:32 +0700

    AURABOT V1.0.0

--- setting/botconfig.js
+++ setting/botconfig.js
@@ -29,7 +29,7 @@ const botResponsePatterns = [
   { command: 'ets', handler: returnCommand["commands_ekstrakAudio"] },
   { command: 'pp', handler: returnCommand["commands_pp"] },
   { command: 'admin', handler: returnCommand["commands_admincontact"] },
-  { command: 'p', handler: returnCommand["commands_pin"] },
+  { command: 'p', handler: returnCommand["commands_pin"].handler },
 
   // Command Admin Only
   { command: 'kick', handler: returnCommand["commands_kick"], isAdmin: true },
