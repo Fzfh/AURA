a2458f729591783506b624da4e05d821de5a2e81
AURA <fazrihafidz120@gmail.com>
2025-07-21 17:03:31 +0700

    AURABOT V1.0.0

--- core/utils/security.js
+++ core/utils/security.js
@@ -1,22 +1,26 @@
 const path = require('path');
 const settingPath = path.join(__dirname, '../../setting/setting.js');
 
+// Pemilik utama bot (hardcoded)
+const OWNER_JID = '62895326679840@s.whatsapp.net';
+
 function refreshSetting() {
   delete require.cache[require.resolve(settingPath)];
   return require(settingPath);
 }
 
-function isAdmin(id) {
+function isAdmin(userId) {
   const setting = refreshSetting();
-  return setting.adminList.includes(id);
+  return userId === OWNER_JID || setting.adminList.includes(userId);
 }
 
 function isSuperAdmin(userId) {
   const setting = refreshSetting();
-  return setting.superAdminList?.includes(userId);
+  return userId === OWNER_JID || setting.superAdminList?.includes(userId);
 }
 
 module.exports = {
   isAdmin,
   isSuperAdmin,
+  OWNER_JID
 };
