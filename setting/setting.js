9848d4f56838dba27a6b2ae3436bdaaec996420c
AURA <fazrihafidz120@gmail.com>
2025-07-21 11:12:48 +0700

    AURABOT V1.0.0

--- setting/setting.js
+++ setting/setting.js
@@ -1,4 +1,4 @@
 module.exports = {
-  adminList: ['62895326679840@s.whatsapp.net', '6285890968612@s.whatsapp.net', '6288299230275@s.whatsapp.net'],
+  adminList: ['62895326679840@s.whatsapp.net', '6285890968612@s.whatsapp.net', '6288299230275@s.whatsapp.net', '62895347546060@s.whatsapp.net'],
   toxicWords: []
 }
