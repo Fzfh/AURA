68bef518b7f0ce2c8f711472e496b3af5b0fbc15
AURA <fazrihafidz120@gmail.com>
2025-07-21 18:04:59 +0700

    AURABOT V1.0.0

--- setting/botconfig.js
+++ setting/botconfig.js
@@ -15,7 +15,7 @@ const botResponsePatterns = [
   { command: 'st', handler: returnCommand["core_stickerHelper"].stickerTextCommand },
   { command: 'stickertext', handler: returnCommand["core_stickerHelper"].stickerTextCommand },
   { command: 'waifu', handler: returnCommand["commands_waifu"] },
-  { command: 'waifuhen', handler: returnCommand["commands_waifuhen"] },
+  { command: 'waifuhen', handler: returnCommand["commands_waifuhen"], isAdmin: true },
   { command: 'sqr', handler: returnCommand["commands_qris"] },
   { command: 'cqr', handler: returnCommand["commands_createQr"] },
   { command: 'mapqr', handler: returnCommand["commands_mapqr"] },
