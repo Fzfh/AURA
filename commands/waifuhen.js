a62b5447ee086f20f2b3b14ba38a2dfec93f02ba
AURA <fazrihafidz120@gmail.com>
2025-08-17 00:24:00 +0700

    AURABOT V1.0.0

--- commands/waifuhen.js
+++ commands/waifuhen.js
@@ -1,10 +1,11 @@
-const { adminList } = require('../setting/setting')
 const axios = require('axios');
 const path = require('path');
-const fs = require('fs');
-const { exec } = require('child_process');
+const { adminList } = require('../setting/setting');
 
-const allowedNSFW = ['ass', 'hentai', 'milf', 'oral', 'paizuri', 'ecchi'];
+// Tags NSFW lengkap dari waifu.im API
+const allowedNSFW = [
+  'ass', 'hentai', 'milf', 'oral', 'paizuri', 'ecchi', 'ero'
+];
 
 module.exports = async function waifuhen(sock, msg, text) {
   try {
@@ -17,7 +18,18 @@ module.exports = async function waifuhen(sock, msg, text) {
       }, { quoted: msg });
     }
 
-    const type = allowedNSFW[Math.floor(Math.random() * allowedNSFW.length)];
+    const type = text?.toLowerCase()?.trim();
+    if (!type) {
+      return sock.sendMessage(sender, {
+        text: `🔞 Gunakan: .waifuhen tag\nTag NSFW tersedia:\n• ${allowedNSFW.join('\n• ')}`
+      }, { quoted: msg });
+    }
+
+    if (!allowedNSFW.includes(type)) {
+      return sock.sendMessage(sender, {
+        text: `❌ Tag *${type}* gak tersedia!\n\nPilih salah satu:\n• ${allowedNSFW.join('\n• ')}`
+      }, { quoted: msg });
+    }
 
     const params = new URLSearchParams({
       included_tags: type,
@@ -25,7 +37,6 @@ module.exports = async function waifuhen(sock, msg, text) {
       gif: 'true',
       limit: '1'
     });
-
     const res = await axios.get(`https://api.waifu.im/search?${params}`, {
       headers: { 'Accept-Version': 'v5' }
     });
@@ -37,35 +48,12 @@ module.exports = async function waifuhen(sock, msg, text) {
     const ext = path.extname(mediaUrl).toLowerCase();
     const caption = `🔞 ${type.charAt(0).toUpperCase() + type.slice(1)} by AuraBot`;
 
-    if (ext === '.gif') {
-      const gifPath = path.join(__dirname, `../temp/${Date.now()}.gif`);
-      const mp4Path = gifPath.replace('.gif', '.mp4');
-
-      const writer = fs.createWriteStream(gifPath);
-      const response = await axios.get(mediaUrl, { responseType: 'stream' });
-      response.data.pipe(writer);
-
-      await new Promise((resolve, reject) => {
-        writer.on('finish', resolve);
-        writer.on('error', reject);
-      });
-
-      await new Promise((resolve, reject) => {
-        const cmd = `ffmpeg -y -i "${gifPath}" -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" "${mp4Path}"`;
-        exec(cmd, (err) => {
-          if (err) return reject(err);
-          resolve();
-        });
-      });
-
+    if (['.gif', '.mp4', '.webm'].includes(ext)) {
       await sock.sendMessage(sender, {
-        video: { url: mp4Path },
+        video: { url: mediaUrl },
         caption,
         gifPlayback: true
       }, { quoted: msg });
-
-      fs.unlinkSync(gifPath);
-      fs.unlinkSync(mp4Path);
     } else {
       await sock.sendMessage(sender, {
         image: { url: mediaUrl },
